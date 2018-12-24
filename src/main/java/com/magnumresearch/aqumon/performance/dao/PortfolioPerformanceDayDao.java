84dbf87e0c9ef663c2d5f8d61cf24303a47a5afb
xiaolong.yu <xiaolong.yu@ufinity.com>
2018-12-24 15:19:57 +0800

    fix typo

--- src/main/java/com/magnumresearch/aqumon/performance/dao/PortfolioPerformanceDayDao.java
+++ src/main/java/com/magnumresearch/aqumon/performance/dao/PortfolioPerformanceDayDao.java
@@ -3,7 +3,9 @@ package com.magnumresearch.aqumon.performance.dao;
 import com.magnumresearch.aqumon.performance.model.PortfolioPerformanceDay;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PortfolioPerformanceDayDao extends JpaRepository<PortfolioPerformanceDay, Long> {
 
     PortfolioPerformanceDay findFirstByPortfolioIdOrderByReportTimeDesc(Long portfolioId);
--- src/main/java/com/magnumresearch/aqumon/performance/web/PerformanceController.java
+++ src/main/java/com/magnumresearch/aqumon/performance/web/PerformanceController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@EnableAutoConfiguration
 @RequestMapping("/performance")
 public class PerformanceController {
 
